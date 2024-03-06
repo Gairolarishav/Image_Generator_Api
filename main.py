7e84cbd9a548fb58909ba0f7bccd30abbbfbcac3
Gairolarishav <101463798+Gairolarishav@users.noreply.github.com>
2024-03-06 20:50:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import openai
 import os
 from pydantic import BaseModel
 import requests
+import base64
+from BingImageCreator import ImageGen
 
 app = FastAPI()
-openai.api_key = os.environ.get('api_key')
+openai.api_key = "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"
 
 # Configure CORS
 app.add_middleware(
@@ -20,33 +22,67 @@ app.add_middleware(
 
 class ImageInput(BaseModel):
     text: str
-    count : int
-    size: str
+    # count : int
+    # size: str
 
 @app.post('/image_generator')
 def Image_Generator(input_data: ImageInput):
-    if input_data.text:
-        images = []
-        response = openai.images.generate(
-            model="dall-e-3",
-            prompt=input_data.text,
-            n=input_data.count,
-            size=input_data.size,
-            quality="standard",
-            response_format='b64_json'
-        )
-        # print(response)
-        try:
-            for i, image_data in enumerate(response.data):
-                image_url = image_data.b64_json
-                images.append(image_url)
-            return images
-        except openai.AuthenticationError as e:
-            raise HTTPException(status_code=401, detail="Incorrect API key")
-        except Exception as e:
-            raise HTTPException(status_code=500, detail="An error occurred while generating images")
-    else:
-        return "Please Enter something"
+    try :    
+       if input_data.text:
+          images= [ ]
+          image_gen = ImageGen(auth_cookie='1Vd9x_vBWsb_L3wJtLXMeKVWOKLCiIG-lPzivgKrqP24RpEfPy4JOoFRFWWTBEHkVA0JOxfWe6GiOeXnYhDRggtZokuRhF7BokBXzt6YMwzeTZS72VINEBgAbOCdLpDaLVIMNxzKVl_D3rLNzeeYYxQBp9itDoK93X8q1xMhxtLr0EwMPlXw2khy-i4FRCK1ZH-ZKohtC2vgYGkGIjKDAIw',auth_cookie_SRCHHPGUSR='1Vd9x_vBWsb_L3wJtLXMeKVWOKLCiIG-lPzivgKrqP24RpEfPy4JOoFRFWWTBEHkVA0JOxfWe6GiOeXnYhDRggtZokuRhF7BokBXzt6YMwzeTZS72VINEBgAbOCdLpDaLVIMNxzKVl_D3rLNzeeYYxQBp9itDoK93X8q1xMhxtLr0EwMPlXw2khy-i4FRCK1ZH-ZKohtC2vgYGkGIjKDAIw')
+
+          # Provide a prompt (your text description)
+          prompt = input_data.text
+
+          # Generate an image based on the prompt
+          image_url = image_gen.get_images(prompt)
+          for i in image_url:
+            response = requests.get(i)
+            if response.status_code == 200:
+                image_bytes = response.content
+                base64_bytes = base64.b64encode(image_bytes)
+                base64_string = base64_bytes.decode('utf-8')
+                print(base64_string)
+                if base64_string.startswith('/'):
+                   images.append(base64_string)
+          return images
+       else:
+         return "Please Enter something"
+    except Exception as e:
+        # print(e.http_status)
+        return (e)
+
+# @app.post('/save_image')
+# def save_image():
+# save_images(links: list, output_dir: str, file_name: str = None) -> None
+
+
+
+# def Image_Generator(input_data: ImageInput):
+#     try :    
+#        if input_data.text:
+#            images = []
+#            response = openai.images.generate(
+#            model="dall-e-2",
+#            prompt= input_data.text,
+#            size= input_data.size,
+#            quality="standard",
+#            response_format = "b64_json",
+#            n=input_data.count,
+#            )
+#            print(response)
+
+#            for i, image_data in enumerate(response.data):
+#                    image_url = image_data.b64_json
+#                    images.append(image_url)
+#                    print(images)
+#            return images
+#        else:
+#          return "Please Enter something"
+#     except openai.OpenAIError as e:
+#         # print(e.http_status)
+#         return (e.code)
     
 
 
@@ -60,6 +96,7 @@ def Content_Generator(input_data: ContentInput):
     messages = [
         {'role': 'system', 'content': 'You are a kind helpful assistant'}
     ]
+    print(input_data)
 
     while True:
       user_input = input_data.text
